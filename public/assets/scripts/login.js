2da2cbff5fb10c0a262086ea030f92b8fe70d754
Joaquin Rivera <u202110072@upc.edu.pe>
2023-11-19 15:11:51 -0500

    Update login.js

--- public/assets/scripts/login.js
+++ public/assets/scripts/login.js
@@ -1,17 +1,3 @@
 function redirect() {
-    // Obtener los valores de los campos de nombre de usuario y contraseña
-    var nombreUsuario = document.getElementById("nombre").value;
-    var contrasena = document.getElementById("contrasena").value;
-    var mensajeError = document.getElementById("mensajeError");
-
-    // Verificar si los campos están en blanco
-    if (nombreUsuario === "" || contrasena === "") {
-        mensajeError.textContent = "Por favor, completa todos los campos antes de iniciar sesión.";
-    } else {
-        // Limpiar el mensaje de error si los campos no están en blanco
-        mensajeError.textContent = "";
-
-        // Si los campos no están en blanco, realizar la redirección
         window.location.href = "../pages/complete_info.html";
-    }
 }
