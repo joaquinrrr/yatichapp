19ce473521e5ccb64c39f371a79c089a85953287
Joaquin Rivera <u202110072@upc.edu.pe>
2023-11-20 09:52:45 -0500

    update final login

--- public/assets/scripts/login.js
+++ public/assets/scripts/login.js
@@ -1,3 +1,22 @@
-function redirect() {
+function validarForm() {
+    var nombre = document.getElementById("nombre").value;
+    var contrasena = document.getElementById("contrasena").value;
+
+    if (nombre === "" || contrasena === "") {
+        alert("Por favor, completa todos los campos.");
+        return false; 
+    }
+
     window.location.href = "../pages/complete_info.html";
+
+    return false; 
 }
+
+
+
+
+
+
+
+
+
